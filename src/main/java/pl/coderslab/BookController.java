7cf7f8c10b953b0cfde016b2fdf184f0c529384a
MattyLevart <mateusz.myrda@gmail.com>
2024-06-07 10:29:46 +0200

    metoda wyświetlająca wszystkie ksiązki

--- src/main/java/pl/coderslab/BookController.java
+++ src/main/java/pl/coderslab/BookController.java
@@ -1,21 +1,31 @@
 package pl.coderslab;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/books")
 public class BookController {
-    private MockBookService mockBookService;
+    private BookService bookService;
 
-    public BookController(MockBookService mockBookService){
-        this.mockBookService = mockBookService;
+    public BookController(BookService bookService){
+        this.bookService = bookService;
     }
     @RequestMapping("/helloBook")
     public Book helloBook() {
         return new Book(1L, "9788324631766", "Thinking in Java",
                 "Bruce Eckel", "Helion", "programming");
     }
+    @GetMapping("")
+    @ResponseBody
+    public List<Book> getList(){
+        return bookService.getBooks();
+    }
+
 
 
 
--- src/main/java/pl/coderslab/MockBookService.java
+++ src/main/java/pl/coderslab/MockBookService.java
@@ -1,26 +1,32 @@
 package pl.coderslab;
 
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 @Component
+@Service
 public class MockBookService implements BookService{
 
-    private List<Book> list;
+    private List<Book> books;
     public MockBookService() {
-        list = new ArrayList<>();
-        list.add(new Book(1L, "9788324631766", "Thinking in Java", "Bruce	Eckel", "Helion", "programming"));
-        list.add(new Book(2L, "9788324627738", "Rusz	glowa	Java.", "Sierra	Kathy,	Bates	Bert", "Helion",
+        books = new ArrayList<>();
+        books.add(new Book(1L, "9788324631766", "Thinking in Java", "Bruce	Eckel", "Helion", "programming"));
+        books.add(new Book(2L, "9788324627738", "Rusz	glowa	Java.", "Sierra	Kathy,	Bates	Bert", "Helion",
                 "programming"));
-        list.add(new Book(3L, "9780130819338", "Java	2.	Podstawy", "Cay	Horstmann,	Gary	Cornell", "Helion",
+        books.add(new Book(3L, "9780130819338", "Java	2.	Podstawy", "Cay	Horstmann,	Gary	Cornell", "Helion",
                 "programming"));
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
     }
 
     @Override
     public List<Book> getBooks() {
-        return null;
+        return books;
     }
 
     @Override
